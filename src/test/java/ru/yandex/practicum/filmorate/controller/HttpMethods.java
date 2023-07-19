c10aa55efb00606930986f26f6761a09354637c6
Artem Smirnov <kaban1106@yandex.ru>
2023-07-19 23:06:08 +0300

    1-st Version

--- src/test/java/ru/yandex/practicum/filmorate/controller/HttpMethods.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/HttpMethods.java
@@ -8,6 +8,7 @@ import java.net.http.HttpResponse;
 
 public class HttpMethods {
     private final String serverUrl;
+
     public HttpMethods(String serverUrl) {
         this.serverUrl = serverUrl;
 
@@ -15,14 +16,14 @@ public class HttpMethods {
 
     public HttpResponse<String> del(String parameters) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
-        URI url = URI.create(serverUrl+parameters);
+        URI url = URI.create(serverUrl + parameters);
         HttpRequest request = HttpRequest.newBuilder().uri(url).DELETE().build();
         return client.send(request, HttpResponse.BodyHandlers.ofString());
     }
 
     public HttpResponse<String> post(String parameters, String json) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
-        URI url = URI.create(serverUrl+parameters);
+        URI url = URI.create(serverUrl + parameters);
         final HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofString(json);
         HttpRequest request = HttpRequest.newBuilder().uri(url).header("Content-Type", "application/json").POST(body).build();
         return client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -30,7 +31,7 @@ public class HttpMethods {
 
     public HttpResponse<String> put(String parameters, String json) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
-        URI url = URI.create(serverUrl+parameters);
+        URI url = URI.create(serverUrl + parameters);
         final HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofString(json);
         HttpRequest request = HttpRequest.newBuilder().uri(url).header("Content-Type", "application/json").PUT(body).build();
         return client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -38,7 +39,7 @@ public class HttpMethods {
 
     public HttpResponse<String> get(String parameters) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
-        URI url = URI.create(serverUrl+parameters);
+        URI url = URI.create(serverUrl + parameters);
         HttpRequest request = HttpRequest.newBuilder().uri(url).GET().build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         return response;
