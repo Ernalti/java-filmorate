230be65597da8a5ce44aedfe1e799b7bceb60586
Artem Smirnov <kaban1106@yandex.ru>
2023-07-19 22:58:56 +0300

    1-st Version

--- src/main/java/ru/yandex/practicum/filmorate/exeption/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exeption/ValidationException.java
@@ -1,7 +1,9 @@
 package ru.yandex.practicum.filmorate.exeption;
 
 public class ValidationException extends RuntimeException {
-    public ValidationException(final String message) {
+
+    public ValidationException(String message) {
         super(message);
     }
+
 }
