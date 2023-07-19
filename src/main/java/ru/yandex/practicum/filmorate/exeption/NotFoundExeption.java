57b9564a2ae3c737c55264d686190ab1c3f3f175
Artem Smirnov <kaban1106@yandex.ru>
2023-07-19 22:51:25 +0300

    1-st Version

--- src/main/java/ru/yandex/practicum/filmorate/exeption/NotFoundExeption.java
+++ src/main/java/ru/yandex/practicum/filmorate/exeption/NotFoundExeption.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exeption;
 
-public class NotFoundExeption extends RuntimeException{
+public class NotFoundExeption extends RuntimeException {
     public NotFoundExeption(String message) {
         super(message);
     }
