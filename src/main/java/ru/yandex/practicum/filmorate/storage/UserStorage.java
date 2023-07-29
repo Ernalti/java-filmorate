ea107aef58d894a76c3e5a9ac538dbd9fdc3273f
Artem Smirnov <kaban1106@yandex.ru>
2023-07-29 05:11:38 +0300

    1-st version

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -7,10 +7,17 @@ import java.util.List;
 public interface UserStorage {
 
     public List<User> getAllUsers();
+
     public User getUserById(Integer id);
+
     public User createUser(User user);
+
     public User updateUser(User user);
+
     public User addFriend(User user, Integer friendId);
+
     public User delFriend(User user, Integer friendId);
+
     public void clearUsers();
+
 }
