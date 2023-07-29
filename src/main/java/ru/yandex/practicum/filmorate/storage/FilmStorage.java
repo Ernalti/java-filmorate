416dceee7cb51d8951e275086550d09325555f13
Artem Smirnov <kaban1106@yandex.ru>
2023-07-29 05:09:57 +0300

    1-st version

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -7,10 +7,17 @@ import java.util.List;
 public interface FilmStorage {
 
     public List<Film> getAllFilms();
+
     public Film createFilm(Film film);
+
     public Film updateFilm(Film film);
+
     public Film addLike(Film film, Integer userId);
+
     public Film delLike(Film film, Integer userId);
+
     public void clearFilms();
+
     public Film getFilmById(Integer id);
+
 }
