0d961072cbedf3edc95ede2d4a31e3ef4374839a
Ernalti <126362213+Ernalti@users.noreply.github.com>
2023-08-10 00:29:37 +0300

    Add files via upload

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -11,7 +11,6 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.io.IOException;
 import java.net.http.HttpResponse;
 import java.time.LocalDate;
-import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -45,7 +44,6 @@ public class UserControllerTest {
                 .name("UserName")
                 .email("user@mail.ru")
                 .birthday(LocalDate.of(1895,12,28))
-                .friends(new HashSet<>())
                 .build();
         HttpResponse<String> response = httpMethods.post("/users", gson.toJson(user));
         assertEquals(200,response.statusCode());
@@ -63,7 +61,6 @@ public class UserControllerTest {
                 .login("UserLogin")
                 .email("user@mail.ru")
                 .birthday(LocalDate.of(1895,12,28))
-                .friends(new HashSet<>())
                 .build();
         HttpResponse<String> response = httpMethods.post("/users", gson.toJson(user));
         assertEquals(200,response.statusCode());
@@ -83,7 +80,6 @@ public class UserControllerTest {
                 .name("UserName")
                 .email("user@mail.ru")
                 .birthday(LocalDate.of(1895,12,28))
-                .friends(new HashSet<>())
                 .build();
         HttpResponse<String> response = httpMethods.post("/users", gson.toJson(user));
         assertEquals(400,response.statusCode());
@@ -99,7 +95,6 @@ public class UserControllerTest {
                 .name("UserName")
                 .email("user@mail.ru")
                 .birthday(LocalDate.of(1895,12,28))
-                .friends(new HashSet<>())
                 .build();
         HttpResponse<String> response = httpMethods.post("/users", gson.toJson(user));
         assertEquals(400,response.statusCode());
@@ -116,7 +111,6 @@ public class UserControllerTest {
                 .name("UserName")
                 .email("ern@ltinaor@mail.ru")
                 .birthday(LocalDate.of(1895,12,28))
-                .friends(new HashSet<>())
                 .build();
         HttpResponse<String> response = httpMethods.post("/users", gson.toJson(user));
         assertEquals(400,response.statusCode());
@@ -132,7 +126,6 @@ public class UserControllerTest {
                 .name("UserName")
                 .email("user@mail.ru")
                 .birthday(LocalDate.of(2025,12,28))
-                .friends(new HashSet<>())
                 .build();
         HttpResponse<String> response = httpMethods.post("/users", gson.toJson(user));
         assertEquals(400,response.statusCode());
@@ -148,7 +141,6 @@ public class UserControllerTest {
                 .name("UserName")
                 .email("user@mail.ru")
                 .birthday(LocalDate.of(1895,12,28))
-                .friends(new HashSet<>())
                 .build();
         httpMethods.post("/users", gson.toJson(user));
         User[] users = gson.fromJson(httpMethods.get("/users").body(), User[].class);
@@ -159,7 +151,6 @@ public class UserControllerTest {
                 .name("UserName")
                 .email("user1@mail.ru")
                 .birthday(LocalDate.of(1995,12,28))
-                .friends(new HashSet<>())
                 .build();
         HttpResponse<String> response = httpMethods.put("/users", gson.toJson(user2));
         assertEquals(200,response.statusCode());
@@ -176,7 +167,6 @@ public class UserControllerTest {
                 .name("UserName")
                 .email("user@mail.ru")
                 .birthday(LocalDate.of(1895,12,28))
-                .friends(new HashSet<>())
                 .build();
         httpMethods.post("/users", gson.toJson(user));
         User user2 = User.builder()
@@ -185,132 +175,12 @@ public class UserControllerTest {
                 .name("UserName")
                 .email("user1@mail.ru")
                 .birthday(LocalDate.of(1995,12,28))
-                .friends(new HashSet<>())
                 .build();
         HttpResponse<String> response = httpMethods.put("/users", gson.toJson(user2));
-        assertEquals(404,response.statusCode());
+        assertEquals(500,response.statusCode());
         User[] users = gson.fromJson(httpMethods.get("/users").body(), User[].class);
         assertEquals(1,users.length);
         user.setId(users[0].getId());
         assertEquals(user.toString(),users[0].toString());
     }
-
-    @Test
-    public void shouldAddFriendToUser() throws IOException, InterruptedException {
-        User user1 = User.builder()
-                .login("User1Login")
-                .name("User1Name")
-                .email("user1@mail.ru")
-                .birthday(LocalDate.of(1990, 6, 15))
-                .friends(new HashSet<>())
-                .build();
-        HttpResponse<String> response1 = httpMethods.post("/users", gson.toJson(user1));
-        assertEquals(200, response1.statusCode());
-        User user2 = User.builder()
-                .login("User2Login")
-                .name("User2Name")
-                .email("user2@mail.ru")
-                .birthday(LocalDate.of(1995, 8, 23))
-                .friends(new HashSet<>())
-                .build();
-        HttpResponse<String> response2 = httpMethods.post("/users", gson.toJson(user2));
-        assertEquals(200, response2.statusCode());
-
-        User[] users = gson.fromJson(httpMethods.get("/users").body(), User[].class);
-        assertEquals(2, users.length);
-
-        HttpResponse<String> response = httpMethods.put("/users/" + users[0].getId() + "/friends/" + users[1].getId(), "");
-        assertEquals(200, response.statusCode());
-
-        User updatedUser1 = gson.fromJson(httpMethods.get("/users/" + users[0].getId()).body(), User.class);
-        User updatedUser2 = gson.fromJson(httpMethods.get("/users/" + users[1].getId()).body(), User.class);
-
-        assertEquals(1, updatedUser1.getFriends().size());
-        assertEquals(1, updatedUser2.getFriends().size());
-        assertEquals(users[1].getId(), updatedUser1.getFriends().iterator().next());
-        assertEquals(users[0].getId(), updatedUser2.getFriends().iterator().next());
-    }
-
-    @Test
-    public void shouldDeleteFriendFromUser() throws IOException, InterruptedException {
-        User user1 = User.builder()
-                .login("User1Login")
-                .name("User1Name")
-                .email("user1@mail.ru")
-                .birthday(LocalDate.of(1990, 6, 15))
-                .friends(new HashSet<>())
-                .build();
-        HttpResponse<String> response1 = httpMethods.post("/users", gson.toJson(user1));
-        assertEquals(200, response1.statusCode());
-        User user2 = User.builder()
-                .login("User2Login")
-                .name("User2Name")
-                .email("user2@mail.ru")
-                .birthday(LocalDate.of(1995, 8, 23))
-                .friends(new HashSet<>())
-                .build();
-        HttpResponse<String> response2 = httpMethods.post("/users", gson.toJson(user2));
-        assertEquals(200, response2.statusCode());
-
-        User[] users = gson.fromJson(httpMethods.get("/users").body(), User[].class);
-        assertEquals(2, users.length);
-
-        HttpResponse<String> addFriendResponse = httpMethods.put("/users/" + users[0].getId() + "/friends/" + users[1].getId(), "");
-        assertEquals(200, addFriendResponse.statusCode());
-
-        HttpResponse<String> delFriendResponse = httpMethods.del("/users/" + users[0].getId() + "/friends/" + users[1].getId());
-        assertEquals(200, delFriendResponse.statusCode());
-
-        User updatedUser1 = gson.fromJson(httpMethods.get("/users/" + users[0].getId()).body(), User.class);
-        User updatedUser2 = gson.fromJson(httpMethods.get("/users/" + users[1].getId()).body(), User.class);
-
-        assertEquals(0, updatedUser1.getFriends().size());
-        assertEquals(0, updatedUser2.getFriends().size());
-    }
-
-    @Test
-    public void shouldGetCommonFriends() throws IOException, InterruptedException {
-        User user1 = User.builder()
-                .login("User1Login")
-                .name("User1Name")
-                .email("user1@mail.ru")
-                .birthday(LocalDate.of(1990, 6, 15))
-                .friends(new HashSet<>())
-                .build();
-        HttpResponse<String> response1 = httpMethods.post("/users", gson.toJson(user1));
-        assertEquals(200, response1.statusCode());
-        User user2 = User.builder()
-                .login("User2Login")
-                .name("User2Name")
-                .email("user2@mail.ru")
-                .birthday(LocalDate.of(1995, 8, 23))
-                .friends(new HashSet<>())
-                .build();
-        HttpResponse<String> response2 = httpMethods.post("/users", gson.toJson(user2));
-        assertEquals(200, response2.statusCode());
-        User user3 = User.builder()
-                .login("User3Login")
-                .name("User3Name")
-                .email("user3@mail.ru")
-                .birthday(LocalDate.of(1987, 4, 10))
-                .friends(new HashSet<>())
-                .build();
-        HttpResponse<String> response3 = httpMethods.post("/users", gson.toJson(user3));
-        assertEquals(200, response3.statusCode());
-
-        User[] users = gson.fromJson(httpMethods.get("/users").body(), User[].class);
-        assertEquals(3, users.length);
-
-        httpMethods.put("/users/" + users[0].getId() + "/friends/" + users[1].getId(), "");
-        httpMethods.put("/users/" + users[0].getId() + "/friends/" + users[2].getId(), "");
-        httpMethods.put("/users/" + users[1].getId() + "/friends/" + users[2].getId(), "");
-
-        HttpResponse<String> commonFriendsResponse = httpMethods.get("/users/" + users[0].getId() + "/friends/common/" + users[1].getId());
-        assertEquals(200, commonFriendsResponse.statusCode());
-
-        User[] commonFriends = gson.fromJson(commonFriendsResponse.body(), User[].class);
-        assertEquals(1, commonFriends.length);
-        assertEquals(users[2].getId(), commonFriends[0].getId());
-    }
 }
-
